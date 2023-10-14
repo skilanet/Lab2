e6c560768681ad3162a47a837790d1cd3b2b9e08
Filonyuk Sergey <filonuk05@gmail.com>
2023-10-14 19:19:31 +0700

    для отчёта

--- main.R
+++ main.R
@@ -3,11 +3,11 @@ library('tseries')
 library('forecast')
 
 # Загрузка таблицы и переименование столбцов
-data <- read.table('Lab2.csv', header = TRUE, sep = ';')[c(1, 2, 3, 4)]
-colnames(data) <- c('year', 'month', 't', 'y')
+data <- read.table('Lab2.csv', header = TRUE, sep = ';')[c(3, 4)]
+colnames(data) <- c('t', 'y')
 
 # Выбираем неообходимый столбец
-tsData <- data[, 4]
+tsData <- data[, 2]
 # Преобразуем во временной ряд
 tsData <- ts(tsData, frequency = 12, start = c(2007, 1))
 # Построение графика временного ряда
@@ -18,22 +18,6 @@ axis(side = 2, at = seq(0, 200, 10), las = 1)
 # Раскладываем временной ряд на основные компоненты
 tsDataComponents <- decompose(tsData)
 plot(tsDataComponents)
-
-# По графику временного ряда можно понять, что модель нелинейная
-myfit1 <- lm(y ~ t, data)
-myfit1
-plot(data$y, type = 'l')
-lines(fitted(myfit1), col = 'red')
-r1 <- residuals(myfit1)
-plot(r1, type = 'l')
-
-# Подгоняем нелинейную модель
-myfit2 <- nls(formula = y ~ (k + a * b^t), data = data, start = c(k = 184.853, a = 214.455, b = 0.965))
-myfit2
-plot(data$y, type = 'l')
-lines(fitted(myfit2), col = 'red')
-r2 <- residuals(myfit2)
-plot(r2, type = 'l')
 
 # Функция для расчёта характеристик остатков
 mypr <- function(x, y) {
@@ -48,15 +32,47 @@ mypr <- function(x, y) {
   RMSE <- sqrt(mean(x * x))
   DC1 <- 1 - sum(x^2) / sum((y - mean(y))^2)
   DC2 <- sum((y - x - mean(y))^2) / sum((y - mean(y))^2)
-  return(c(n = n, sd = SD, Min = Min, Max = Max, ME = ME, MAE = MAE, MPE = MPE, MAPE = MAPE, RMSE = RMSE, DC1 = DC1, DC2 = DC2))
+  return(data.frame(n = n, sd = SD, Min = Min, Max = Max, ME = ME, MAE = MAE, MPE = MPE, MAPE = MAPE, RMSE = RMSE, DC1 = DC1, DC2 = DC2))
 }
 
-# Вычисляем характеристики остатков
-mypr(r2, data$y)
+# Подгоняем нелинейную модель
+myfit1 <- nls(formula = y ~ (k - a * b^t), data = data, start = c(k = 184.853, a = 214.455, b = 0.965))
+myfit1
+r1 <- residuals(myfit1)
+plot(data$y, type = 'l')
+lines(data$y - r1, col = 'red', lwd = 2)
+sp1 <- spec.pgram(r1, detrend = FALSE, log = 'no', fast = FALSE, pad = FALSE, taper = 0, plot = TRUE)
+ac <- Acf(r1, lag.max = 15, plot = TRUE, na.action = na.contiguous, demean = TRUE)
+plot(ac, yaxt = 'n')
+axis(side = 2, at = seq(-0.2, 1.2, 0.05), las = 1)
+Pacf(r1, lag.max = 15, plot = TRUE)
+options(digits = 5)
+mypr(r1, data$y)
 
-# Вычисляем значения периодаграммы и строим график
-sp <- spec.pgram(tsData, detrend = FALSE, log = 'no', fast = FALSE, pad = FALSE, taper = 0, plot = TRUE)
-# Вычисляем и строим график автокорреляционной функции
-tsAcf <- Acf(tsData, lag.max = 15, plot = TRUE, na.action = na.contiguous, demean = TRUE)
-# Вычисляем и строим график частотной автокорреляционной функции
-tsPacf <- pacf(tsData, lag.max = 15, plot = TRUE)
+r2 <- residuals(myfit1)
+ts2 <- ts(r2, frequency = 12, start = c(2007, 1))
+plot.ts(ts2)
+sp2 <- spec.pgram(ts2, detrend = FALSE, log = 'no', fast = FALSE, pad = FALSE, taper = 0, plot = TRUE)
+plot(sp)
+Acf(ts2, lag.max = 36)
+Pacf(ts2, lag.max = 36)
+m2 <- nls(y ~ k / (1 + a * exp(b * t)) +
+  a1 * cos(2 * pi * t / 12) +
+  b1 * sin(2 * pi * t / 12) +
+  a2 * cos(6 * pi * t / 12) +
+  b2 * sin(6 * pi * t / 12), data, start = c(k = 180, a = 15, b = -0.1, a1 = 0.1, b1 = 0.1, a2 = 0.1, b2 = 0.1))
+m2
+r3 <- residuals(m2)
+plot(data$y, type = 'l')
+lines(data$y - r3, col = 'red', lwd = 2)
+mypr(r3, data$y)
+
+ts3 <- ts(r3, frequency = 12, start = c(2007, 1))
+plot.ts(ts3)
+sp3 <- spec.pgram(ts3, detrend = FALSE, log = 'no', fast = FALSE, pad = FALSE, taper = 0, plot = TRUE)
+Acf(ts3, lag.max = 15, plot = TRUE, na.action = na.contiguous, demean = TRUE)
+Pacf(ts3, lag.max = 15, plot = TRUE)
+
+
+
+
